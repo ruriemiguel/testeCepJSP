abc54ed9fe1af9f2c11478c314f4dbac3c90aa36
Ruriê <rurie.miguel@gmail.com>
2022-11-13 17:29:44 -0300

    V. Final

---
+++ src/main/java/endereco/Endereco.java
@@ -0,0 +1,74 @@
+package endereco;
+
+public class Endereco {
+    
+    String cep;
+    String logradouro;
+    String complemento;
+    String bairro;
+    String localidade;
+    String uf;
+    String ddd;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "logradouro=" + logradouro + ", bairro=" + bairro + ", localidade=" + localidade + '}';
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<div>
+		<input required type="text" name="CEP" placeholder='Entre com o seu CEP' id="CEP"><br>
+		
+		<input required type="text" name="Logradouro" placeholder='Logradouro' id="Logradouro"><br>
+		<input required type="text" name="Complemento" placeholder='Complemento' id="Complemento" value=><br>
+		<input required type="text" name="Bairro" placeholder='Bairro' id="Bairro" value=><br>
+		<input required type="text" name="Localidade" placeholder='Localidade' id="Localidade" value=><br>
+		<input required type="text" name="UF" placeholder='UF' id="UF" value=><br>
+		<input required type="text" name="DDD" placeholder='DDD' id="DDD" value=><br>
+	</div>
+</body>
+</html>
